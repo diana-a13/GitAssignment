5d2f73bc1ca1fba4514fe4cc1c37304ee75a0277
Diana <Diana@DESKTOP-HCNIH7N>
2020-03-20 00:34:33 +0200

    First commit

---
+++ GitAssignment/src/ro/ase/csie/cts/g1074/TestGit.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1074;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este 'Aplicatie mobila - lunch sharing'");
+	}
+
+}
