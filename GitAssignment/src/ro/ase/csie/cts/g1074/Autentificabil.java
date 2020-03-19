5ecdea37f8a974e517d951764656cbaa993a5180
Diana <Diana@DESKTOP-HCNIH7N>
2020-03-20 00:55:22 +0200

    Adaugare interfata si implementarea in main

---
+++ GitAssignment/src/ro/ase/csie/cts/g1074/Autentificabil.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1074;
+
+public interface Autentificabil {
+	public void login(String email, String parola);
+}
--- GitAssignment/src/ro/ase/csie/cts/g1074/TestGit.java
+++ GitAssignment/src/ro/ase/csie/cts/g1074/TestGit.java
@@ -4,6 +4,8 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git ! Denumirea proiectului de licenta este 'Aplicatie mobila - lunch sharing'");
-	}
 
+		User u = new User();
+		u.login("exemplu.@gmail.com", "parolaExemplu");
+	}
 }
